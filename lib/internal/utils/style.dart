b39f1aa6bc85ea627dab9197ff1adf2f39652f19
dinhdung140198 <dinhdung.jr7@gmail.com>
2023-04-13 22:14:15 +0700

    style color

--- lib/internal/utils/style.dart
+++ lib/internal/utils/style.dart
@@ -14,13 +14,13 @@ class AppTextStyle {
         color: AppColors.titleColor,
         fontWeight: FontWeight.w500,
       );
-  
+
   static TextStyle get normal => montserrat.copyWith(
         fontSize: 20.0,
         color: AppColors.titleColor,
         fontWeight: FontWeight.w300,
       );
-  
+
   static TextStyle get content => montserrat.copyWith(
         fontSize: 16.0,
         color: AppColors.titleColor,
@@ -37,7 +37,14 @@ class AppTextStyle {
 class AppColors {
   static const Color backgroundColor = Color(0xFF0B0F2F);
   static const Color mainColor = Color(0xFF3E60F9);
+  static const Color titleColor = Color(0xFFFFFFFF);
   static const Color blueEFF = Color(0xFF449EFF);
   static const Color blue7F7 = Color(0xFF1DC7F7);
-  static const Color titleColor = Color(0xFFFFFFFF);
+  static const Color blueAE7 = Color(0xFF48CAE7);
+  static const Color blue8F8 = Color(0xFF3D58F8);
+  static const Color blueBA1 = Color(0xFF2C4BA1);
+  static const Color blue577 = Color(0xFF1E3577);
+  static const Color greyDBD = Color(0xFFABADBD);
+  static const Color grey76A = Color(0xFF42476A);
+  static const Color greyD3B = Color(0xFF151D3B);
 }
